a539864ec9fe507dd98302f3d2526187409ab0f9
Andrew White <awhite@simplymeasured.com>
2017-01-21 09:51:30 -0600

    basic RGB and input processing

---
+++ gpio_listener.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+bcm_pins = [25, 8, 7, 11]
+
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+
+    for pin in bcm_pins:
+        GPIO.setup(pin, GPIO.IN)
+
+
+def read_gpio():
+    prev_states = [False, False, False, False]
+    states = [False, False, False, False]
+
+    while True:
+        for index, pin in enumerate(bcm_pins):
+            states[index] = GPIO.input(pin)
+
+        if any(states) and states == prev_states:
+            return states
+
+        prev_states = list(states)
+        time.sleep(0.1)
---
+++ remote.py
@@ -0,0 +1,46 @@
+from flux_led import WifiLedBulb
+import sys
+
+
+def read_test_input():
+    return [True, False, False, False]
+
+
+def handle_rgb_state(bulbs, state):
+    r = 255 if state[1] else 0
+    g = 255 if state[2] else 0
+    b = 255 if state[3] else 0
+
+    for bulb in bulbs:
+        bulb.setRgb(r, g, b)
+
+
+def handle_custom_states(bulbs, state):
+    if not any(state[1:]):
+        for bulb in bulbs:
+            bulb.setWarmWhite255(255)
+
+
+def handle_state(bulbs, state):
+    if not state[0]:
+        handle_rgb_state(bulbs, state)
+    else:
+        handle_custom_states(bulbs, state)
+
+
+def main():
+    ip_addresses = sys.argv[1:]
+    bulbs = [WifiLedBulb(ip_address) for ip_address in ip_addresses]
+
+    prev_state = [False, False, False, False]
+    while True:
+        state = read_test_input()
+        if state == prev_state:
+            continue
+            
+        handle_state(bulbs, state)
+        prev_state = list(state)
+
+
+if __name__ == "__main__":
+    main()
